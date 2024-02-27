c6ae2593f5f984bc2bcf39b7daa4b0f6b396f554
poornimadevi22 <poornikutty02@gmail.com>
2024-02-27 20:36:43 +0530

    added changes

--- mentortask/models/student.js
+++ mentortask/models/student.js
@@ -4,6 +4,6 @@ const studentSchema = new mongoose.Schema({
   name:String,
   emailId:String,
   class:Number,
-  mentors:[Number]
+  mentors:[String]
 });
 module.exports = mongoose.model('students', studentSchema);
